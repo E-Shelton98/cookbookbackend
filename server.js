2d2c11344b5ccc841ead1a95a6fc1238ed343862
Erik Shelton <erikshelton@Eriks-Air.lan1>
2020-10-20 00:43:53 -0400

    finished backend setup

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+//Environmental Variables
+require('dotenv').config()
+const { PORT = 4000, NODE_ENV = "development"} = process.env
+
+//mongo connection
+const mongoose = require('./db/connection')
+
+//cors
+const cors = require('cors')
+const corsOptions = require('./configs/cors.js')
+
 //import express
 const express = require('express');
 //create app
@@ -6,9 +17,15 @@ const app = express();
 const logger = require('morgan');
 
 // Add the middleware code needed to accept incoming data and add it to req.body
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//Route for testing that server is working
+app.get('/', (req, res) => {
+    res.json({ hello: "Hello World!" })
+})
 
 const cookbookRouter = require('./controllers/cookbookRoutes');
 app.use('/api/cookbooks/', cookbookRouter);
